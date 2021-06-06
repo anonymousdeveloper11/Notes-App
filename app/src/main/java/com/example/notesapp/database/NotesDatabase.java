7e26292425c184d0bc9d56078f2e4c4ece9aa4a4
binoddangi123 <binoddangi46@gmail.com>
2021-06-06 18:29:33 +0545

    first commit

---
+++ app/src/main/java/com/example/notesapp/database/NotesDatabase.java
@@ -0,0 +1,28 @@
+package com.example.notesapp.database;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.example.notesapp.dao.NoteDao;
+import com.example.notesapp.entities.Note;
+
+@Database(entities = Note.class, version = 1, exportSchema = false)
+public abstract class NotesDatabase extends RoomDatabase {
+
+    public static NotesDatabase notesDatabase;
+    public static synchronized NotesDatabase getDatabase(Context context){
+        if(notesDatabase == null){
+            notesDatabase= Room.databaseBuilder(
+                    context,
+                    NotesDatabase.class,
+                    "notes_db"
+            ).build();
+        }
+        return notesDatabase;
+    }
+
+    public abstract NoteDao noteDao();
+}
